a6c5008ca430a3af69fd51928a344f888fc8b5e2
MrEmgin <bakin.d@liceum1535.ru>
2022-10-07 01:37:07 +0300

    EntertainmentCenter class is inder construction implemented init working on the loading from and saving to CSV file

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import csv
+import datetime
+
+
+class ListOperator:
+    pass
+
+
+class EntertainmentCenter:
+    def __init__(self):
+        self.music = ListOperator(Track)
+        self.books = ListOperator(Book)
+        self.games = ListOperator(Game)
+        self.movies = ListOperator(Movie)
+        self.path_to_file = str(datetime.datetime.now().time())
+        pass
+
+    def load(self, path_to_file='data/sample.csv'):
+        if not os.path.exists(path_to_file):
+            raise FileNotFoundError(f"File {path_to_file} not found.")
+        self.path_to_file = path_to_file
+        with open(path_to_file, newline='') as file:
+            reader = csv.reader(file, delimiter=',', quotechar='|')
+            for row in reader:
+                if row[0] == '1':
+                    self.books.add(Book(row[1:]))
+                elif row[0] == '2':
+                    self.music.add(Track(row[1:]))
+                elif row[0] == '3':
+                    self.music.add(Game(row[1:]))
+                elif row[0] == '4':
+                    self.books.add(Movie(row[1:]))
+                else:
+                    raise ValueError("CSV file is not correct")
+
+    def save(self):
+        for collection in (self.music, self.books, self.games, self.movies):
+            for inst in collection:
+                inst.save(self)
+
+
+BOOK: '<type:1 - book>,<name>,<author>,<year>,<genre>,<ration>,<age_restriction>,<filename>'
+
+
+# method from Media class
+def save(self, center_inst, obj_inst, *args):
+    if obj_inst.__class__ not in [Book, Track, Movie, Game]:
+        raise TypeError(f'Expected from [Book, Track, Movie, Game], got {obj_inst.__class__} instead')
+    with open(center_inst.path_to_file, mode='w', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='|')
+        data = []
+        if isinstance(obj_inst, Book):
+            data.append('1')
+        elif isinstance(obj_inst, Track):
+            data.append('2')
+        elif isinstance(obj_inst, Game):
+            data.append('3')
+        elif isinstance(obj_inst, Movie):
+            data.append('4')
+        data.extend([obj_inst.id, obj_inst.name, obj_inst.author, obj_inst.year,
+                     obj_inst.genre, obj_inst.rating, obj_inst.age_restriction, *args])
+        writer.writerow(list(map(str, data)))
+
+
+if __name__ == '__main__':
+    print(EntertainmentCenter().__class__)
