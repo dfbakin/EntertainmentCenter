5f8f04a4d88a3b7e8296c25ce81418c9398c2284
Nickolas-option <n.evkarpidi@innopolis.university>
2022-10-08 03:19:22 +0300

    Added Games class, added type annotations

--- Media.py
+++ Media.py
@@ -14,7 +14,9 @@ class Media:
 	# age_restriction -> int  # возрастное ограничение
 	"""
 
-	def __init__(self, name="Some media", author="Ivan Ivanov", year=0, genre="", rating=0, age_restriction=21):
+	def __init__(self, name: str = "Some media", author: str = "Ivan Ivanov", year: int = 0, genre: str = "",
+	             rating: int = 0,
+	             age_restriction: int = 21):
 		if not isinstance(name, str):
 			raise TypeError("Name is not a string")
 		self.name = name
@@ -47,67 +49,67 @@ class Media:
 		return self.name
 
 	@name.setter
-	def name(self, new_name):
+	def name(self, new_name: str):
 		if isinstance(new_name, int):
 			raise TypeError("The name must be a string.")
 
-		self.name = new_name
+		self._name = new_name
 
 	@property
 	def author(self):
 		return self.author
 
 	@author.setter
-	def author(self, new_author):
+	def author(self, new_author: str):
 		if isinstance(new_author, str):
 			raise TypeError("Author must be a str.")
-		self.author = new_author
+		self._author = new_author
 
 	@property
 	def year(self):
 		return self.year
 
 	@year.setter
-	def year(self, new_year):
+	def year(self, new_year: int):
 		if isinstance(new_year, int):
 			raise TypeError("The year must be an integer.")
 		if new_year < 0:
 			raise ValueError("The year must be a positive integer.")
-		self.year = new_year
+		self._year = new_year
 
 	@property
 	def genre(self):
 		return self.genre
 
 	@genre.setter
-	def genre(self, new_genre):
+	def genre(self, new_genre: str):
 		if isinstance(new_genre, str):
 			raise TypeError("Genre must be a string.")
-		self.genre = new_genre
+		self._genre = new_genre
 
 	@property
 	def rating(self):
 		return self.rating
 
 	@rating.setter
-	def rating(self, new_rating):
+	def rating(self, new_rating: int):
 		if isinstance(new_rating, int):
 			raise TypeError("Rating must be an integer.")
 		if new_rating > 100 or new_rating < 0:
 			raise ValueError("Rating is not in [0, 100] interval.")
-		self.rating = new_rating
+		self._rating = new_rating
 
 	@property
 	def age_restriction(self):
 		return self.age_restriction
 
 	@age_restriction.setter
-	def age_restriction(self, new_age_restriction):
+	def age_restriction(self, new_age_restriction: str):
 		if isinstance(new_age_restriction, int):
 			raise TypeError("Age_restriction must be an integer.")
 		if new_age_restriction < 0 or new_age_restriction > 21:
 			raise ValueError("Age restriction is not appropriate.")
-		self.age_restriction = new_age_restriction
+		self._age_restriction = new_age_restriction
 
 	def __str__(self):
 		return f"A media with name: {self.name} authored by {self.author} in genre of {self.genre} and with rating of " \
